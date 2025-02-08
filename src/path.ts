9aa44965bd93b1d139abed93dd16e178929e5335
codenoobforreal <panjunyu1222@foxmail.com>
2025-02-08 20:20:59 +0800

    update tsconfig to include bun recommended config

--- src/path.ts
+++ src/path.ts
@@ -2,5 +2,5 @@ import "dotenv/config";
 
 import process from "node:process";
 
-export const defaultInputDir = process.env.DEFAULTINPUTDIR as string;
-export const defaultOutputDir = process.env.DEFAULTOUTPUTDIR as string;
+export const defaultInputDir = process.env["DEFAULTINPUTDIR"] as string;
+export const defaultOutputDir = process.env["DEFAULTOUTPUTDIR"] as string;
